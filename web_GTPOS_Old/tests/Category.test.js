7970adeccd9e62a88ba01e47b7ed35c39699ccaa
Trupti Vasava <trupti.vasava@eil.local>
2025-11-17 12:15:44 +0530

    12345

--- web_GTPOS_Old/tests/Category.test.js
+++ web_GTPOS_Old/tests/Category.test.js
@@ -1,6 +1,6 @@
 describe('wer Login', () => {
     describe('Web Form' , () => {
-        it('should add Batch', async () =>{
+        it('123456', async () =>{
             await browser.url ('http://qa.gtpos.ethicstechnology.net/');
 
             const username = await $('[name ="userId"]');
